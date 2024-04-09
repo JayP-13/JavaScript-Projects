cb75b7ce351e9b68a65c79b4519a481717c08e9c
Jay <Jamestp81382@hotmail.com>
2024-04-09 00:26:46 -0400

    JS assignment

--- Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -1,1 +1,7 @@
-document.write("Hello, World");
+var Sent1 = "This is the beginning of the string"; //Creating var1 with a string value
+var Sent2 = "and this is the end of the string"; //Creating var2 with a string vaule
+var Concatenate = Sent1 + Sent2; //Concatenating Var1 and Var2
+    window.alert(Concatenate); //Using Window Alert to show the strings together
+
+var result = (21 * 75) //Creating an expression
+    document.write(result); //Using Document write method to show the equation result
