e37e64e92dd119f8e3621ffd92d5df87fa95de90
Jay <Jamestp81382@hotmail.com>
2024-04-09 21:35:25 -0400

    Assigment

---
+++ Project2_functions/JS/main.js
@@ -0,0 +1,10 @@
+     function myFunction() { //Creating a function that will use the += operator to create a string
+      var string = "This is a test for the +=";
+      string += "operator for my TA assignment";
+      return string;
+    }
+
+document.getElementById("demo").innerHTML = myFunction(); //This will call the function and show it inteh HTML element that is "demo"
+
+
+
